d88ce7ab8d0ab98c30af644dd8a9278c940422ef
rosa7788 <rosennigt7@gmail.com>
2026-08-12 20:24:35 -0300

    exe

---
+++ semestre2/bubbleSortObj.mjs
@@ -0,0 +1,18 @@
+function bubbleSort(vetor, fnComp){
+    let trocou
+    do{
+        trocou = false
+        
+        for(let i = 0; i < vetor.length - 1; i++){
+            if(fnComp(vetor[i], vetor[i+1])){
+                [vetor[i], vetor[i+1] = [vetor[i+1]], vetor[i]]
+            }
+        }
+    }while(trocou)
+}
+
+import {objMotorista} from 
+
+bubbleSort(objMotorista, (elem1, elem2) ==> elem1.nome_motorista > elem2.nome_motorista)
+
+console.log()
